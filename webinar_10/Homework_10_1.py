d9ee3f6b8aab78379a65d40ce1b0355903ebef04
Mikhail Ivanov <example8921@gmail.com>
2023-06-19 11:04:26 +0300

    Задание к 10 вебинару с небольшими правками

--- webinar_10/Homework_10_1.py
+++ webinar_10/Homework_10_1.py
@@ -1,20 +1,20 @@
 import random
 
 
+def generate_word():
+    """
+    Создает случайный набор букв на латинице длиной до 15 символов
+    :return: строка длиной до 15 символов
+    """
+    word_length = random.randint(1, 15)
+    word = ''.join(chr(random.randint(ord('a'), ord('z'))) for _ in range(word_length))
+    return word
+
+
 def generate_random_name():
     """
     Генерирует две случайных последовательности букв на латинице длиной до 15 символов в нижнем регистре
     """
-
-    def generate_word():
-        """
-        Создает случайный набор букв на латинице длиной до 15 символов
-        :return: строка длиной до 15 символов
-        """
-        word_length = random.randint(1, 15)
-        word = ''.join(chr(random.randint(ord('a'), ord('z'))) for _ in range(word_length))
-        return word
-
     while True:
         yield f'{generate_word()} {generate_word()}'
 
